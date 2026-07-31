debf8c5b637a705614f9b144961673be678238fd
David Dombrowsky <davek@6thstreetradio.org>
2026-07-31 11:02:41 -0400

    need to update create.sh

--- create.sh
+++ create.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 #
 
-docker run -d --name agenttest --volume ./v:/opt ubuntu sleep infinity
+# TODO: this should come from the agenttest:latest image and use
+# something supervisord or --init, plus /opt/master_agent/emperor.sh
+docker run -d --init --name agenttest \
+    --volume ./v:/opt \
+    agenttest:latest \
+    supervisord -c /etc/supervisor/supervisord.conf -n
