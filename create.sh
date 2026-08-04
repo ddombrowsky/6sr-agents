5878810d95016b5d69f5c98e031392cf079c10ba
David Dombrowsky <davek@6thstreetradio.org>
2026-08-04 10:42:11 -0400

    notes about docker creation script

--- create.sh
+++ create.sh
@@ -1,9 +1,27 @@
 #!/bin/sh
 #
 
-# TODO: this should come from the agenttest:latest image and use
-# something supervisord or --init, plus /opt/master_agent/emperor.sh
-docker run -d --init --name agenttest \
+# this relies on this file being in the image:
+#
+# /etc/supervisor/conf.d/emperor.conf
+#
+# [program:emperor]
+# command=/opt/master_agent/emperor.sh
+# directory=/opt/master_agent
+# autostart=true
+# autorestart=true
+# startsecs=1
+# stdout_logfile=/dev/fd/1
+# stdout_logfile_maxbytes=0
+# stderr_logfile=/dev/fd/2
+# stderr_logfile_maxbytes=0
+# stopsignal=TERM
+# stopasgroup=true
+# killasgroup=true
+
+# -n flag = nodaemon, required to exist as init proc
+
+docker run -d --name agenttest \
     --volume ./v:/opt \
     agenttest:latest \
     supervisord -c /etc/supervisor/supervisord.conf -n
